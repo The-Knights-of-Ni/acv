e5049c757536b98e818237f9081e61a4d3df1834
Ashwin Naren <arihant2math@gmail.com>
2024-02-01 09:24:54 -0800

    more shenanigans

---
+++ android-sys/build.rs
@@ -0,0 +1,47 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let sys_root_path = PathBuf::from("C:/Users/ariha/AppData/Local/Android/Sdk/ndk/26.0.10792818/toolchains/llvm/prebuilt/windows-x86_64/sysroot");
+    let include_dir_path = sys_root_path.join("usr").join("include")
+        // Canonicalize the path as `rustc-link-search` requires an absolute
+        // path.
+        .canonicalize()
+        .expect("cannot canonicalize path");
+
+    let shared_lib_path = sys_root_path.join("usr").join("lib").join("arm-linux-androideabi")
+        // Canonicalize the path as `rustc-link-search` requires an absolute
+        // path.
+        .canonicalize()
+        .expect("cannot canonicalize path");
+
+    let headers_path_str = "wrapper.h";
+
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search={}", shared_lib_path.to_str().unwrap());
+
+    // Tell cargo to tell rustc to link our library.
+    // println!("cargo:rustc-link-lib=log");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header(headers_path_str)
+        .clang_arg(format!("-I{}", include_dir_path.to_str().unwrap()))
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
+    bindings
+        .write_to_file(out_path)
+        .expect("Couldn't write bindings!");
+}
