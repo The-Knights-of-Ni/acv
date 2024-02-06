939b4b6d6adabb00704dca766fd2151365027c9a
Ashwin Naren <arihant2math@gmail.com>
2024-02-06 09:03:11 -0800

    updates

--- acv/src/frame_generator.rs
+++ acv/src/frame_generator.rs
@@ -1,8 +1,12 @@
 use image::Rgb;
 use imageproc::definitions::Image;
 
-#[cfg(feature = "jni")]
+#[cfg(feature = "camera-jni")]
 pub mod jni;
+
+#[cfg(feature = "camera-ndk")]
+pub mod ndk;
+mod ndk;
 
 pub trait FrameGenerator {
     fn frame(&mut self) -> crate::Result<Image<Rgb<u8>>>;
